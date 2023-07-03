0c19115294463cba6a6eab978e4d83bdd421019a
Kimyongba <kimyongba@gmail.com>
2023-07-03 20:30:08 +0900

    #4.9 Login Form fix

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
-
-import '../../constants/sizes.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
-
   @override
   State<LoginFormScreen> createState() => _LoginFormScreenState();
 }
 
 class _LoginFormScreenState extends State<LoginFormScreen> {
-  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Map<String, String> formData = {};
-
   void _onSubmitTap() {
-    if (_formkey.currentState != null) {
-      if (_formkey.currentState!.validate()) {
-        _formkey.currentState!.save();
-        print(formData);
+    if (_formKey.currentState != null) {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsScreen(),
+          ),
+        );
       }
     }
   }
@@ -36,43 +37,74 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
           horizontal: Sizes.size36,
         ),
         child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                Gaps.v28,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Email',
+          key: _formKey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
                   ),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['email'] = newValue;
-                    }
-                  },
-                ),
-                Gaps.v16,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
                   ),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['password'] = newValue;
-                    }
-                  },
                 ),
-                Gaps.v28,
-                GestureDetector(
-                    onTap: _onSubmitTap,
-                    child: const FormButton(disabled: false, text: "Log in"))
-              ],
-            )),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Plase write your email";
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Plase write your password";
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: const FormButton(
+                  disabled: false,
+                  text: 'Next',
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
